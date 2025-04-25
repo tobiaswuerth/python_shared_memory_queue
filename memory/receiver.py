f6fda5ba2b44d4779f93141f9b3d74ad558a0eb1
tobiaswuerth <21124705+tobiaswuerth@users.noreply.github.com>
2025-04-25 07:54:15 +0200

    use pickle for conversion to bytes

--- memory/receiver.py
+++ memory/receiver.py
@@ -1,75 +1,43 @@
 import multiprocessing as mp
-import queue
 from multiprocessing.shared_memory import SharedMemory
 
-from .converter import decode, iterables
+import pickle
 
-iterable_keys = set(iterables.values())
-non_dict_iterables = set(iterables.keys()) - {dict}
 
 class SharedMemoryReceiver:
-    def __init__(self, queue_data_in:mp.Queue, queue_ack_out:mp.Queue):
+    def __init__(self, queue_data_in: mp.Queue, queue_ack_out: mp.Queue):
         self.queue_data_in = queue_data_in
         self.queue_ack_out = queue_ack_out
 
-    def _recover_data_recursive(self, shm:SharedMemory, info:tuple, byte_idx=0):
-        if info[0] not in iterable_keys:
-            if info[1] == 0: # size
-                return decode(None, info)
-            return decode(shm.buf[byte_idx:byte_idx+info[1]], info)
-
-        if info[0] == 'd': # dict
-            data = {}
-            for info_child, key in zip(info[3], info[4]):
-                item = self._recover_data_recursive(shm, info_child, byte_idx)
-                byte_idx += info_child[1]
-                data[key] = item
-            return data
-
-        # tuple, list, set
-        data = []
-        for info_child in info[3]:
-            item = self._recover_data_recursive(shm, info_child, byte_idx)
-            byte_idx += info_child[1]
-            data.append(item)
-        
-        if info[2] in non_dict_iterables:
-            return info[2](data)
-        
-        # NamedTuple needs to be handled separately
-        return info[2](*data)
-
-    def _recover_data(self, info:tuple):
-        if info[2] == 0:
-            return self._recover_data_recursive(None, info[1:])
-
-        name = info[0]
-        info = info[1:]
-        shm = SharedMemory(name=name, size=info[1])
-        data = self._recover_data_recursive(shm, info)
+    def _recover_data(self, info: tuple):
+        # get data
+        name, total_bytes = info[0]
+        shm = SharedMemory(name=name, size=total_bytes)
+        local_buffers = bytes(shm.buf[:total_bytes])
         shm.close()
-        return data
 
-    def get_nowait(self):
-        if self.queue_data_in.empty():
-            return None
-        
-        try:
-            info = self.queue_data_in.get_nowait()
-        except queue.Empty:
-            return None
-        
-        if info is None:
-            return None
-        
+        # unpack data
+        buffers = []
+        offset = 0
+        for length in info[1]:
+            buffer = local_buffers[offset : offset + length]
+            buffers.append(buffer)
+            offset += length
+
+        main = buffers.pop(0)
+        return pickle.loads(main, buffers=buffers)
+
+    def _process_info(self, info: tuple):
+        assert info, "No info received"
         item = self._recover_data(info)
-        self.queue_ack_out.put_nowait(info[0])
+        self.queue_ack_out.put_nowait(info[0][0])
         del info
         return item
+
+    def get_nowait(self):
+        info = self.queue_data_in.get_nowait()
+        return self._process_info(info)
 
     def get(self, timeout=None):
         info = self.queue_data_in.get(timeout=timeout)
-        item = self._recover_data(info)
-        self.queue_ack_out.put_nowait(info[0])
-        del info
-        return item
+        return self._process_info(info)
--- memory/sender.py
+++ memory/sender.py
@@ -1,40 +1,8 @@
 import multiprocessing as mp
 import queue
 from multiprocessing.shared_memory import SharedMemory
-
-from .converter import encode, iterables
-
-class _Separator:
-    def __init__(self, obj):
-        self.obj = obj
-
-def _iterable_handler_list(stack, parents, entry):
-    stack.append(_Separator((type(entry), entry.__class__,)))
-    parents.append(([], []))
-    for field in reversed(entry): # reverse to maintain order in final binary
-        stack.append(field)
-
-def _iterable_handler_set(stack, parents, entry):
-    stack.append(_Separator((set,entry.__class__,)))
-    parents.append(([], []))
-    for field in entry:
-        stack.append(field)
-
-def _iterable_handler_dict(stack, parents, entry):
-    keys = list(entry.keys())
-    stack.append(_Separator((dict,entry.__class__,keys)))
-    parents.append(([], []))
-    for key in reversed(keys): # reverse to maintain order in final binary
-        stack.append(entry[key])
-
-iterable_handlers = {
-    list: _iterable_handler_list,
-    tuple: _iterable_handler_list,
-    set: _iterable_handler_set,
-    dict: _iterable_handler_dict,
-}
-
-tuple_key = iterables[tuple]
+import pickle
+from pickle import PickleBuffer
 
 class SharedMemorySender:
     def __init__(self, capacity, queue_data_out:mp.Queue, queue_ack_in:mp.Queue):
@@ -44,71 +12,38 @@ class SharedMemorySender:
         self.capacity = capacity
 
     def _prepare_binary(self, data):
-        parents:list[tuple[list[memoryview], list[tuple]]] = []
-        stack = [data]
-        while stack:
-            entry = stack.pop()
-            
-            if isinstance(entry, _Separator):
-                # children processed, gather the results
-                bytes_data, child_infos = parents.pop()
-                entry_type = entry.obj[0]
-                key = iterables.get(entry_type, tuple_key)
-                size = sum(info[1] for info in child_infos)
-                clazz = entry.obj[1]
-                if entry_type == dict:
-                    info = (key, size, clazz, child_infos, entry.obj[2])
-                else:   
-                    info = (key, size, clazz, child_infos)
+        buffers:list[PickleBuffer] = []
+        buffer_lengths:list[int] = []
+        def _append_buffer(buffer:PickleBuffer):
+            memview = buffer.raw()
+            buffers.append(memview)
+            buffer_lengths.append(len(memview))
+        
+        main = pickle.dumps(data, buffer_callback=_append_buffer, protocol=pickle.HIGHEST_PROTOCOL)
 
-                if not parents:
-                    # root entry was tuple, return the result
-                    del parents, stack
-                    return info, bytes_data
+        len_main = len(main)
+        total = len_main + sum(buffer_lengths)
+        buffer_lengths.insert(0, len_main)
+        buffers.insert(0, main)
 
-                # append to parent binary
-                parents[-1][0].extend(bytes_data)
-                parents[-1][1].append(info)
-                continue
+        return total, buffer_lengths, buffers
 
-            entry_type = type(entry)
-            if entry_type in iterables:
-                iterable_handlers[entry_type](stack, parents, entry)
-                continue
-            if isinstance(entry, tuple):
-                # NamedTuple
-                _iterable_handler_list(stack, parents, entry)
-                continue
-
-            info, bytes_data = encode(entry)
-            if not parents:
-                # root entry was single data, return the result
-                del parents, stack
-                return info, [bytes_data]
-
-            parents[-1][0].append(bytes_data)
-            parents[-1][1].append(info)
 
     def put(self, data):
         self.process_ack()
         while self.capacity and len(self.open_handles) >= self.capacity:
             self.wait_for_ack()
 
-        info, bytes_datas = self._prepare_binary(data)
-        size = info[1]
-        if size == 0:
-            info = (None, *info)
-            self.queue_data_out.put(info)
-            return
+        total_bytes, buffer_lengths, buffers = self._prepare_binary(data)
+        assert total_bytes > 0, 'No data to send'
 
-        shm = SharedMemory(create=True, size=size)
-        info = (shm.name, *info)
+        shm = SharedMemory(create=True, size=total_bytes)
+        info = ((shm.name, total_bytes), buffer_lengths)
 
         offset = 0
-        for item in bytes_datas:
-            size = len(item)
-            shm.buf[offset:offset + size] = item
-            offset += size
+        for length, buffer in zip(buffer_lengths, buffers):
+            shm.buf[offset:offset + length] = buffer
+            offset += length
 
         self.queue_data_out.put(info)
         self.open_handles[shm.name] = shm
